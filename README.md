8a44d8a9539965c9898e2e849cca73b739e6c131
Gregório Gadea <gregoriogadea.sl026@academico.ifsul.edu.br>
2023-08-01 22:15:42 -0300

    commit 1

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # aws_sg_ip_change
-This Script changes your ip in a AWS Security Group.
+This Script changes your ip in AWS Security Group.
 
-☝🏻 You can use this script with Leapp.
+## How to use
+- Rembember to have your AWS CLI already configured. (You can use Leapp to do that)
+1. Clone this repository
+2. Install the requirements
+3. Change the variables in the script to attend your needs, like the security group id and the security group roule id
+4. Run the script
--- ipchange.sh
+++ ipchange.sh
@@ -2,6 +2,6 @@ MY_IP=$(curl --silent https://checkip.amazonaws.com)
 echo "Your IP is ${MY_IP}"
 
 aws ec2 modify-security-group-rules \
-    --group-id sg-0777829b1e627e0e0 \            /* <----- Security Group ID */
-    --security-group-rules SecurityGroupRuleId=sgr-0baf5cdc4882737c3,SecurityGroupRule="{IpProtocol=-1,CidrIpv4=${MY_IP}/32}"  /* ID Of Your Security Group Rule and the ip protocol of the rule*/
+    --group-id sg-0777829b1e627e0e0 \     
+    --security-group-rules SecurityGroupRuleId=sgr-0baf5cdc4882737c3,SecurityGroupRule="{IpProtocol=-1,CidrIpv4=${MY_IP}/32}"
 
