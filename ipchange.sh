20f6faadf24ed75c9c9b5fca0ebd58ad46db22a5
Gregório Gadea <gregoriogadea.sl026@academico.ifsul.edu.br>
2023-08-03 11:17:29 -0300

    edit script

--- ipchange.sh
+++ ipchange.sh
@@ -4,5 +4,5 @@ MY_IP=$(curl --silent https://checkip.amazonaws.com)
 echo "Your IP is ${MY_IP}"
 
 aws ec2 modify-security-group-rules \
-    --group-id sg-09c03ce5bcd3906e4 \
-    --security-group-rules SecurityGroupRuleId=sgr-05fc3d27f978aadc1,SecurityGroupRule="{IpProtocol=-1,CidrIpv4=${MY_IP}/32}"
+    --group-id sg-09d03ce5bcd3904e5 \
+    --security-group-rules SecurityGroupRuleId=sgr-05gy3d27f978aadc1,SecurityGroupRule="{IpProtocol=-1,CidrIpv4=${MY_IP}/32}"
